81f2f16f887dac878b3c1c2cadb0d63d18adfbbb
kainaturooj <128933130+kainaturooj@users.noreply.github.com>
2023-09-10 01:41:02 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+////// import, links//////////////////////
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js"
+
+// import { getDatabase, set, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js"
+
+// Initialize Firebase
+const auth = getAuth()
+
+// const dataBase = getDatabase()
+
+
+////////////admin authentication//////////////
+//student authentication///////////////////////////
+/////////////// student data set//////////////////
---
+++ config.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDcWphG_JF9vPYNxs0dI4z0KGx09w_Tdik",
+    authDomain: "attendence-app-178c0.firebaseapp.com",
+    projectId: "attendence-app-178c0",
+    storageBucket: "attendence-app-178c0.appspot.com",
+    messagingSenderId: "967432384100",
+    appId: "1:967432384100:web:23e487fc6fbd5145daf971",
+    measurementId: "G-ZDL1S9MF9Z"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
